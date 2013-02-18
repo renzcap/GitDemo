0bcdff0ae18c8bcdf97343d089200ef2cd7fb136
Laurence Capitan <renzcap@yahoo.com>
2013-02-18 21:48:22 +0800

    added thirdVC

---
+++ GitSample/GitSample/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  GitSample
+//
+//  Created by Laurence Capitan on 2/18/13.
+//  Copyright (c) 2013 Nextsprout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
