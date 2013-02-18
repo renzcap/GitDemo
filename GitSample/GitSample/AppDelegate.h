165013558afbbd06dfca020960b7334edc8b8f58
Laurence Capitan <renzcap@yahoo.com>
2013-02-18 21:36:13 +0800

    Added Project

---
+++ GitSample/GitSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitSample
+//
+//  Created by Laurence Capitan on 2/18/13.
+//  Copyright (c) 2013 Nextsprout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitSample/GitSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitSample
+//
+//  Created by Laurence Capitan on 2/18/13.
+//  Copyright (c) 2013 Nextsprout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
