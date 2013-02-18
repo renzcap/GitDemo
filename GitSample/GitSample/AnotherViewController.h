3365ae4ad08a76364919ed0420baa6a5a1b5ffbc
Laurence Capitan <renzcap@yahoo.com>
2013-02-18 21:46:40 +0800

    Added anotherVC

---
+++ GitSample/GitSample/AnotherViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnotherViewController.h
+//  GitSample
+//
+//  Created by Laurence Capitan on 2/18/13.
+//  Copyright (c) 2013 Nextsprout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnotherViewController : UIViewController
+
+@end
